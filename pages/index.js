859d4d8022d2665b3836b3a49e149db37b0e25dc
wehttqm <matthewfalcone5@gmail.com>
2024-03-07 03:21:32 -0500

    can search by track id

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,16 @@ class Home extends Component {
             '3aUp8U4MNWctRXlY5MPysv',
             '7BdBAeVMiVHbszGoN7WsAt',
             '5EWPGh7jbTNO2wakv8LjUI',
+            '2aJDlirz6v2a4HREki98cP'
         ],
         
     }
     getSongs = () => {
-        console.log('no songs to get since THIS ISNT IMPLEMENTED')
+        // console.log('no songs to get since THIS ISNT IMPLEMENTED')
+        if (this.state.text != '') {
+            this.setState({song_ids: [this.state.text]})
+        }
+
     }
     render() {
         return (
