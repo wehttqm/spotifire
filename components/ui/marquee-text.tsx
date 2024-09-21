6bfa7ec90e22dec887221644fb6602b37077682e
wehttqm <matthewfalcone5@gmail.com>
2024-09-21 13:04:38 -0400

    fixed a bug in marquee text by using a completely different approach

--- components/ui/marquee-text.tsx
+++ components/ui/marquee-text.tsx
@@ -1,34 +1,42 @@
 "use client"
 import { useEffect, useRef, useState } from "react";
+
+const getWidth = (text: string) => {
+    // gets the width of the text we are about to render. If > 400, then we need sliding text. Otherwise, we just render the text normally. 
+    const tempContainer = document.createElement('div');
+    tempContainer.style.visibility = 'hidden'; 
+    tempContainer.style.position = 'absolute'; 
+    document.body.appendChild(tempContainer); 
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = text;
+    tempElement.className = 'font-bold text-2xl'
+    tempContainer.appendChild(tempElement); 
+    const width = tempElement.offsetWidth;
+    document.body.removeChild(tempContainer);
+
+    return width;
+}
+
 export const MarqueeText = ({ text }: { text: string }) => {
     const test = useRef<any>()
     const [isAnimated, setAnimated] = useState(false)
+    const width = getWidth(text)
 
-    useEffect(() => {
-        setAnimated(false)
-        if (test) {
-            try {
-                const isLongEnough = test.current.offsetWidth > 400
-                setAnimated(isLongEnough)
-            } catch (err) {
-                setAnimated(false)
-            }
-            
-        } 
-    }, [text, test])
-
+    if (width > 400) {
         return (
-            ( isAnimated ? 
-                <div className='marquee marquee--fit-content w-[400px] mt-2'>
-                    <div className='marquee__content'>
-                        <span className='font-bold text-2xl'>{text}</span>
-                    </div>
-                    <div className='marquee__content' aria-hidden={true}>
-                        <span className="font-bold text-2xl">{text}</span>
-                    </div>
-                </div> 
-            :
-                <div ref={test} className='mt-2 text-2xl font-bold whitespace-nowrap overflow-hidden'>{text}</div>
-            )
+            <div className='marquee marquee--fit-content w-[400px] mt-2'>
+                <div className='marquee__content'>
+                    <span className='font-bold text-2xl'>{text}</span>
+                </div>
+                <div className='marquee__content' aria-hidden={true}>
+                    <span className="font-bold text-2xl">{text}</span>
+                </div>
+            </div> 
         )
+    }
+    return (
+        <div className='mt-2 text-2xl font-bold whitespace-nowrap overflow-hidden'>{text}</div>
+    )
+
 }
+
